00975e6e19e56a68489d87c85f6663ae60135786
kabir narang <kabirnarang39@gmail.com>
2021-03-20 12:24:10 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ var peer = new Peer(undefined,{
     host:'/',
     port:'443'
 }); 
-//console.log(user);
+console.log(window.location.href);
 //console.log(videoGrid)
 navigator.mediaDevices.getUserMedia({
     video:true,
