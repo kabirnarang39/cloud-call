e4308b7be29486975826bf3cf1a7728a50b6c062
kabir narang <kabirnarang39@gmail.com>
2021-03-20 12:30:44 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ var peer = new Peer(undefined,{
 }); 
 console.log(window.location.href);
 //console.log(videoGrid)
-console.log(user)
+console.log(user.profileObj)
 navigator.mediaDevices.getUserMedia({
     video:true,
     audio:true
