9f4f84661c6c0d557e3664341dba81936449019c
kabir narang <kabirnarang39@gmail.com>
2021-03-24 00:15:28 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -156,6 +156,7 @@ function Area(Increment, Count, Width, Height, Margin = 10) {
   
   // Load and Resize Event
   window.addEventListener("load", function (event) {
+    if(!loading)
     Dish();
   
     window.onresize = Dish;
