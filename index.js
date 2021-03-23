25837752ed35bdf0b789944b41463e13582ca1a9
kabir narang <kabirnarang39@gmail.com>
2021-03-23 22:27:41 +0530

    make it better44344344344344232345

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ io.on('connection',socket=>{
     socket.on('join-room',(roomId,userId)=>{
         socket.join(roomId);
        // console.log('joined')
-        socket.to(roomId).broadcast.emit('user-connected',userId)
+       // socket.to(roomId).broadcast.emit('user-connected',userId)
         socket.to(roomId).broadcast.emit('screen-connected',userId)
        socket.on('screen-data',(data)=>{
        data=JSON.parse(data);
