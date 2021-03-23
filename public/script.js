92855d18fa3a8a98fa70bf193aee7bf50423f6ae
kabir narang <kabirnarang39@gmail.com>
2021-03-23 23:17:35 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -308,24 +308,16 @@ function toggleVid() {
     }
 }*/
 const setScreen=(screen)=>{
-    navigator.getUserMedia = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia;
-
-if (navigator.getUserMedia) {
-navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
-function(stream) {
-var video = document.querySelector('video');
-video.srcObject = stream;
-video.onloadedmetadata = function(e) {
-video.play();
-};
-},
-function(err) {
-console.log("The following error occurred: " + err.name);
-}
-);
-} else {
-console.log("getUserMedia not supported");
-}
+    navigator.mediaDevices.getDisplayMedia().then(stream => {
+        return stream;
+    }).then(stream => {
+          console.log(myVideoStream.getVideoTracks()[0])
+          myVideoStream.getVideoTracks()[0].enabled=false;
+          const video=document.createElement('video');
+          video.srcObject=stream;
+          video.onloadedmetadata = function(e) {
+            video.play();
+          };
+          document.querySelector(".screen_share").append(video);  
+})
 }
