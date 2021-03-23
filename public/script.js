08a8c86d83bab47421a18b933b7a0278a09d89c9
kabir narang <kabirnarang39@gmail.com>
2021-03-24 00:35:21 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -320,10 +320,10 @@ function setScreen() {
          console.log(peers)
             for (let socket_id in peers) {
                 console.log("peers[socket_id].streams[0].getTracks() ", peers[socket_id].remoteStream.getTracks())
-                for (let index in peers[socket_id].streams[0].getTracks()) {
+                for (let index in peers[socket_id].remoteStream.getTracks()) {
                     for (let index2 in stream.getTracks()) {
-                        if (peers[socket_id].streams[0].getTracks()[index].kind === stream.getTracks()[index2].kind) {
-                            peers[socket_id].replaceTrack(peers[socket_id].streams[0].getTracks()[index], stream.getTracks()[index2], peers[socket_id].streams[0])
+                        if (peers[socket_id].remoteStream.getTracks()[index].kind === stream.getTracks()[index2].kind) {
+                            peers[socket_id].replaceTrack(peers[socket_id].remoteStream.getTracks()[index], stream.getTracks()[index2], peers[socket_id].remoteStream.getTracks())
                             break;
                         }
                     }
@@ -338,11 +338,11 @@ function setScreen() {
                 // document.querySelector(".screen_sharing").style.display = "none";
                 navigator.mediaDevices.getUserMedia(constraints).then(stream => {
                     for (let socket_id in peers) {
-                        for (let index in peers[socket_id].streams[0].getTracks()) {
+                        for (let index in peers[socket_id].remoteStream.getTracks()) {
                             for (let index2 in stream.getTracks()) {
                                 if (peers[socket_id].streams[0].getTracks()[index].kind === stream.getTracks()[index2].kind) {
                                     console.log("entered")
-                                    peers[socket_id].replaceTrack(peers[socket_id].streams[0].getTracks()[index], stream.getTracks()[index2], peers[socket_id].streams[0])
+                                    peers[socket_id].replaceTrack(peers[socket_id].streams[0].getTracks()[index], stream.getTracks()[index2], peers[socket_id].remoteStream.getTracks())
                                     break;
                                 }
                             }
