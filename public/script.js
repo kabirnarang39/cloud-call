0f36d4f202ce60cda8b7ff5d140e6c3bafd3a8cf
kabir narang <kabirnarang39@gmail.com>
2021-03-26 00:42:18 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -325,6 +325,7 @@ shareScreenBtn.addEventListener("click", (e) => {
     .then((stream) => {
       var videoTrack = stream.getVideoTracks()[0];
       myVideoTrack = myVideoStream.getVideoTracks()[0];
+      setScreenShareDisableButton();
       replaceVideoTrack(myVideoStream, videoTrack);
       for (peer in peers) {
         let sender = peers[peer].peerConnection.getSenders().find(function (s) {
@@ -353,6 +354,7 @@ shareScreenBtn.addEventListener("click", (e) => {
     });
 });
 const stopPresenting = (videoTrack) => {
+    setScreenShareButton();
     shareScreenBtn.classList.remove("true");
     shareScreenBtn.setAttribute("tool_tip", "Present Screen");
     for (peer in peers) {
@@ -372,3 +374,21 @@ const replaceVideoTrack = (stream, videoTrack) => {
     stream.addTrack(videoTrack);
   };
 ////////
+const setScreenShareButton=()=>{
+    const html=`
+    <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" fill="#00796b" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+    <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>
+  </svg>
+    `
+    document.querySelector('.main_screen_button').innerHTML=html;
+}
+const setScreenShareDisableButton=()=>{
+    const html=`
+    <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" fill="#cc3b33" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+    <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>
+  </svg>
+    `
+    document.querySelector('.main_screen_button').innerHTML=html;
+}
