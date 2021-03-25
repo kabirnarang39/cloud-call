690acfb5ebd29586b793d15b17fef03a13500b5f
kabir narang <kabirnarang39@gmail.com>
2021-03-25 18:33:55 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 var socket = io('/');
 let myVideoStream;
 const videoGrid=document.querySelector('.video-grid')
-const screen_Share=document.querySelector('.screen_share');
 const myVideoElement=document.createElement('video')
 const myVideoElementScreen=document.createElement('video')
 myVideoElement.muted=true;
@@ -18,6 +17,54 @@ console.log(JSON.parse(myParam))
 //console.log(window.location.href.split('?')[1])
 //console.log(user);
 //console.log(videoGrid)
+///////////////////////////////////////////////
+const videoElem = document.getElementById("video");
+const logElem = document.getElementById("log");
+const startElem = document.getElementById("start");
+const stopElem = document.getElementById("stop");
+
+// Options for getDisplayMedia()
+
+var displayMediaOptions = {
+  video: {
+    cursor: "always"
+  },
+  audio: false
+};
+
+// Set event listeners for the start and stop buttons
+startElem.addEventListener("click", function(evt) {
+  startCapture();
+}, false);
+
+stopElem.addEventListener("click", function(evt) {
+  stopCapture();
+}, false);
+async function startCapture() {
+    logElem.innerHTML = "";
+  
+    try {
+      videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+      dumpOptionsInfo();
+    } catch(err) {
+      console.error("Error: " + err);
+    }
+  }
+  function stopCapture(evt) {
+    let tracks = videoElem.srcObject.getTracks();
+  
+    tracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
+  }
+  function dumpOptionsInfo() {
+    const videoTrack = videoElem.srcObject.getVideoTracks()[0];
+  
+    console.info("Track settings:");
+    console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+    console.info("Track constraints:");
+    console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+  }
+////////////////////////////////////////
 const closeBtn=()=>{
     var close = document.getElementsByClassName("closebtn");
 var i;
