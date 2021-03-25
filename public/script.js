4d4a4c0c61c4aaabd4e0318395c6c74e8ff869ae
kabir narang <kabirnarang39@gmail.com>
2021-03-26 01:06:16 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -334,7 +334,7 @@ shareScreenBtn.addEventListener("click", (e) => {
         sender.replaceTrack(videoTrack);
       }
       const elementsWrapper = document.querySelector(".elements-wrapper");
-      const stopBtn = document.querySelector('.stop');
+      const stopBtn = document.createElement("button");
       stopBtn.classList.add("video-element");
       stopBtn.classList.add("stop-presenting-button");
       stopBtn.innerHTML = "Stop Sharing";
@@ -385,7 +385,7 @@ const setScreenShareButton=()=>{
 }
 const setScreenShareDisableButton=()=>{
     const html=`
-  <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" fill="#cc3b33" class="bi bi-file-x stop" viewBox="0 0 16 16">
+  <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" fill="#cc3b33" class="bi bi-file-x" viewBox="0 0 16 16">
   <path d="M6.146 6.146a.5.5 0 0 1 .708 0L8 7.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 8l1.147 1.146a.5.5 0 0 1-.708.708L8 8.707 6.854 9.854a.5.5 0 0 1-.708-.708L7.293 8 6.146 6.854a.5.5 0 0 1 0-.708z"/>
   <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
 </svg>
