5163dda1390beee99371e3aecd2e034a0cd982c2
kabir narang <kabirnarang39@gmail.com>
2021-03-25 19:20:29 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const screenshot = require('screenshot-desktop')
+import * as screenshot from 'screenshot-desktop';
 var socket = io('/');
 let myVideoStream;
 const videoGrid=document.querySelector('.video-grid')
