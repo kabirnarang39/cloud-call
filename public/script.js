a14f7740a6964b3ff34cc26bc18b5db5b581377d
kabir narang <kabirnarang39@gmail.com>
2021-03-26 19:45:14 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -215,8 +215,8 @@ videoWrapper.classList.add("video-wrapper");
     video.addEventListener('loadedmetadata',()=>{
         video.play();
     })
-    videoGrid.appendChild(elementsWrapper);
-    videoGrid.appendChild(video);
+    videoWrapper.appendChild(elementsWrapper);
+  videoWrapper.appendChild(video);
    
   // console.log(videoGrid)
 }
