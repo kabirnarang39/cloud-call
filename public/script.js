d232421307d9c67a9d05f173bce713eab37cb0da
kabir narang <kabirnarang39@gmail.com>
2021-03-27 13:03:18 +0530

    make it better44344344344344232345edc

--- public/script.js
+++ public/script.js
@@ -252,8 +252,8 @@ const setStopVideo=()=>{
 }
 const displayChat=()=>{
     const html=`
-    <svg width="26px" height="26px" viewBox="0 0 16 16" class="bi bi-chat-left-fill" fill="#cc3b33" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" fill="#cc3b33" class="bi bi-chat" viewBox="0 0 16 16">
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
           </svg>
     `
     document.querySelector('.main_left').style.flex="0.8";
@@ -264,8 +264,8 @@ const displayChat=()=>{
  }
  const hideChat=()=>{
     const html=`
-    <svg width="26px" height="26px" viewBox="0 0 16 16" class="bi bi-chat-left-fill" fill="#00796b" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" fill="#00796b" class="bi bi-chat" viewBox="0 0 16 16">
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
           </svg>
     `
     document.querySelector('.main_right').style.display="none";
@@ -397,3 +397,25 @@ const meetEnd=()=>{
     window.location.href='https://zoom-12.herokuapp.com/meet-end'
 }
    
+const shareToggleButton = document.getElementById('share-toggle')
+shareToggleButton.addEventListener('click', e => {
+  const dialogue = document.querySelector('.dialogue-container')
+  dialogue.classList.toggle('dialogue-active')
+})
+const copyButton = document.querySelector('#copy-button')
+const shareLink = document.querySelector('.share-link')
+shareLink.innerHTML = window.location.href
+copyButton.setAttribute('meeting_link', window.location.href)
+const dialogueCloseButton = document.querySelector('#close-dialogue')
+dialogueCloseButton.addEventListener('click', (e) => {
+  const dialogue = document.querySelector('.dialogue-container')
+  dialogue.classList.toggle('dialogue-active')
+})
+copyButton.addEventListener("mousedown", (e) => {
+  const copyText = e.target.getAttribute('meeting_link')
+  navigator.clipboard.writeText(copyText);
+  e.target.setAttribute("tool_tip", 'copied');
+});
+copyButton.addEventListener("mouseout", (e) => {
+  e.target.setAttribute("tool_tip", 'copy');
+});
