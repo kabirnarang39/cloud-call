af0215460f6dbb8af4e06bce5332b0e2d31661c9
kabir narang <kabirnarang39@gmail.com>
2021-03-30 13:29:09 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -40,21 +40,20 @@ navigator.mediaDevices.getUserMedia({
 myVideoStream=stream;
 addStream(myVideoElement,stream,null,username,null);
 peer.on('call', call=> {
-  
-  socket.on('user-connected',(userId,username,image,count)=>{
-    call.answer(stream); // Answer the call with an A/V stream.
+  call.answer(stream); // Answer the call with an A/V stream.
   const video=document.createElement('video')
   call.on('stream', (userVideoStream)=> {
     console.log(username)
-  addStream(video,userVideoStream,call.peer,username)
-    //document.querySelector('.flash').innerHTML='User Connected'+userId;
-    connectToNewUser(userId,stream,username);
-    document.querySelector('.flash').innerHTML=(`<div class="alert success"><span class="closebtn" onClick="closeBtn();">&times;</span><strong>${username}</strong> connected.</div>`)
-    //alert('Somebody connected', userId)
-    changeCount(count);
-    
-})
-  
+    socket.on('user-connected',(userId,username,image,count)=>{
+      addStream(video,userVideoStream,call.peer,username)
+      //document.querySelector('.flash').innerHTML='User Connected'+userId;
+      connectToNewUser(userId,stream,username);
+      document.querySelector('.flash').innerHTML=(`<div class="alert success"><span class="closebtn" onClick="closeBtn();">&times;</span><strong>${username}</strong> connected.</div>`)
+      //alert('Somebody connected', userId)
+      changeCount(count);
+      
+  })
+ 
 });
 })
 
