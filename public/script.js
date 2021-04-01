d077c1f3a79deb92d114dcadb2fe4e8203badfa3
kabir narang <kabirnarang39@gmail.com>
2021-04-01 23:06:58 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -664,27 +664,9 @@ const record = (stream) => {
 };
 
 const mouseoverMsg=(message)=>{
-  if(message=='Mic On')
-document.querySelector('.button_message1').innerHTML=message;
-if(message=='Video On')
-document.querySelector('.button_message2').innerHTML=message;
-if(message=='Share Screen')
-document.querySelector('.button_message3').innerHTML=message;
-if(message=='Share Link')
-document.querySelector('.button_message4').innerHTML=message;
-if(message=='Chat')
-document.querySelector('.button_message5').innerHTML=message;
-if(message=='Start Recording')
-document.querySelector('.button_message6').innerHTML=message;
-if(message=='Leave Meeting')
-document.querySelector('.button_message7').innerHTML=message;
+document.querySelector('.button_message').innerHTML=message;
+
 }
 const mouseoutMsg=(message)=>{
-document.querySelector('.button_message1').innerHTML=message;
-document.querySelector('.button_message2').innerHTML=message;
-document.querySelector('.button_message3').innerHTML=message;
-document.querySelector('.button_message4').innerHTML=message;
-document.querySelector('.button_message5').innerHTML=message;
-document.querySelector('.button_message6').innerHTML=message;
-document.querySelector('.button_message7').innerHTML=message;
+  document.querySelector('.button_message').innerHTML=message;
 }
