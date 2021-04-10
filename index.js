980ff8f72da4bdc855b980ee17a651ebd7ce2c70
kabir narang <kabirnarang39@gmail.com>
2021-04-10 16:01:53 +0530

    make it better44344344344344232345

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ io.on("connection", (socket) => {
       });
     });
   });
-  server.listen(process.env.PORT || 3000);
 server.listen(process.env.PORT || 8000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
