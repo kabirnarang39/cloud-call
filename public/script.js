832a8ac78158c36a5b868c4d9c9d3fa98efffce3
kabir narang <kabirnarang39@gmail.com>
2021-04-12 00:32:21 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -626,6 +626,7 @@ const record = (stream) => {
 
 const emojiPicker=()=>{
   document.querySelector('.main_message_container_chat').style.display='block';
+  document.querySelector('.main_message_container_chat').style.zIndex='10000000';
   document.querySelector('.main_message_container_chat').style.margin='20px';
   document.querySelector('.main_message_container_chat').style.overflow='scroll';
   document.querySelector('.main_message_container_chat').style.position='absolute';
