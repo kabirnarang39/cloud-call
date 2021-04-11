5fd9654203b3cbe5c6e3510ad661db124440e46f
kabir narang <kabirnarang39@gmail.com>
2021-04-12 00:33:43 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -629,6 +629,6 @@ const emojiPicker=()=>{
   document.querySelector('.main_message_container_chat').style.zIndex='10000000';
   document.querySelector('.main_message_container_chat').style.margin='20px';
   document.querySelector('.main_message_container_chat').style.overflow='scroll';
-  document.querySelector('.main_message_container_chat').style.position='absolute';
+  document.querySelector('.main_message_container_chat').style.position='relative';
   document.querySelector('.main_message_container_chat').innerHTML=`<emoji-picker style="display:absolute;"></emoji-picker>`;
 }
