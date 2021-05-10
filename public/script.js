414a00d8ea8a598c39c08708d5a5a4b0671ab71e
kabir narang <kabirnarang39@gmail.com>
2021-05-10 14:46:10 +0530

    make it better44344344344344232345

--- public/script.js
+++ public/script.js
@@ -11,6 +11,47 @@ var peer = new Peer(undefined,{
     port:'443'
 });
 var Peer_ID;
+var mousePosition;
+var offset = [0,0];
+var div;
+div=document.getElementsByClassName('main-right');
+var isDown = false;
+div.style.position = "absolute";
+div.style.left = "0px";
+div.style.top = "0px";
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "blue";
+
+document.body.appendChild(div);
+
+
+div.addEventListener('mousedown', function(e) {
+  isDown = true;
+  offset = [
+      div.offsetLeft - e.clientX,
+      div.offsetTop - e.clientY
+  ];
+}, true);
+
+document.addEventListener('mouseup', function() {
+  isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+  event.preventDefault();
+  if (isDown) {
+      mousePosition = {
+  
+          x : event.clientX,
+          y : event.clientY
+  
+      };
+      div.style.left = (mousePosition.x + offset[0]) + 'px';
+      div.style.top  = (mousePosition.y + offset[1]) + 'px';
+  }
+}, true);
 //Loading spinner
 window.onload = function () {
   const loader = document.createElement("div");
