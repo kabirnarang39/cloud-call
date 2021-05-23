46c7b7abeacd337c01c1fbaa08456a1cb11bb364
kabir narang <kabirnarang39@gmail.com>
2021-05-24 02:51:49 +0530

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const mongoDBStore=require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-var cors = require('cors')
 var flash=require("connect-flash")
 const errorController=require('./controllers/error');
 const User=require('./models/user');
@@ -55,10 +54,8 @@ app.use(session({
   saveUninitialized:false,
   store:store
 }))
-
 app.use(csrfProtection);
 app.use(flash());
-
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
